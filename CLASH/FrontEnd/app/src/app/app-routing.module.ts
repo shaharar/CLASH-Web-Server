fe7339b031177def1b23516e1648572f75747f44
shaharar <shaharar@post.bgu.ac.il>
2020-04-26 15:17:59 +0300

    mti-results component

--- CLASH/FrontEnd/app/src/app/app-routing.module.ts
+++ CLASH/FrontEnd/app/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SearchComponent } from './search/search.component';
 import { HomeComponent } from './home/home.component';
+import { MtiResultsComponent } from './mti-results/mti-results.component';
 
 
 const routes: Routes = [
   {path:  "", pathMatch:  "full", redirectTo:  "home"},
   {path: "home", component: HomeComponent},
-  {path: "search", component: SearchComponent}
+  {path: "search", component: SearchComponent},
+  {path: "mti-results", component: MtiResultsComponent}
 ];
 
 @NgModule({
--- CLASH/FrontEnd/app/src/app/app.module.ts
+++ CLASH/FrontEnd/app/src/app/app.module.ts
@@ -6,21 +6,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule} from '@angular/common/http';
 import { from } from 'rxjs';
-import { MtiDetailsComponent } from './mti-details/mti-details.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { SearchComponent } from './search/search.component';
 import { HttpRequestsService } from './http-requests.service';
+import { MtiResultsComponent } from './mti-results/mti-results.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MtiDetailsComponent,
     HeaderComponent,
     FooterComponent,
     HomeComponent,
-    SearchComponent
+    SearchComponent,
+    MtiResultsComponent
   ],
   imports: [
     BrowserModule,
